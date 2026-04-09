c23b97e7b19957e55bd1453a1a923bbc9ca757bb
Fassam Khan <muhammadfassam465@gmail.com>
2026-04-09 20:24:25 +0500

    update

--- component/Hero.jsx
+++ component/Hero.jsx
@@ -10,12 +10,12 @@ import Image from 'next/image'
 const Hero = () => {
   return (
     <div className='w-[100%]  overflow-hidden p-3  border-b-3 border-[var(--primary-color)]'>
-      <Carousel className="w-full relative ">
+      <Carousel className="w-full relative  h-screen">
      
         <CarouselContent>
          
           
-          <CarouselItem className=" w-full bg-[url('/hero1.jpeg')] p-3  bg-cover bg-center   h-[400px]  "  >
+          <CarouselItem className="  w-full bg-[url('/hero1.jpeg')] p-3 bg-no-repeat bg-cover bg-center    "  >
             <div className='border flex flex-col gap-4 h-full justify-center  !p-8 bg-[#00000067]'>
               <h1 className='md:text-5xl text-3xl  font-[Orbitron] text-white font-bold'>YOUR ROUTE TO SUCCESS</h1>
               <p className='text-white md:text-2xl text-lg font-semibold'>Premium Truck Dispatch Services</p>
@@ -31,13 +31,13 @@ const Hero = () => {
 
           </CarouselItem>
            
-          <CarouselItem className=" w-full bg-[url('/hero1.jpeg')] p-3  bg-cover bg-center   h-[400px]  "  >
+          <CarouselItem className="w-full bg-[url('/hero2.jpeg')] p-3 bg-no-repeat  bg-cover bg-center   h-screen "  >
             <div className='border flex flex-col gap-4 h-full justify-center  !p-8 bg-[#00000067]'>
-              <h1 className='md:text-5xl text-3xl  font-[Orbitron] text-white font-bold'>YOUR ROUTE TO SUCCESS</h1>
-              <p className='text-white md:text-2xl text-lg font-semibold'>Premium Truck Dispatch Services</p>
+              <h1 className='md:text-5xl text-3xl  font-[Orbitron] text-white font-bold'>Find High-Paying Loads 24/7</h1>
+              <p className='text-white md:text-2xl text-lg font-semibold'>Expert dispatchers working arround the clock</p>
               <button className="bg-[var(--primary-color)] text-white px-8 !py-2 skew-x-[20deg] md:w-[250px] w-[180px]">
                 <span className="block skew-x-[-20deg] font-bold">
-                  <a href="">GET STARTED TODAY</a>
+                  <a href="">VIEW SERVICES</a>
                 </span>
               </button>
 
@@ -46,6 +46,23 @@ const Hero = () => {
 
 
           </CarouselItem>
+
+          <CarouselItem className="w-full bg-[url('/hero3.jpeg')] p-3 bg-no-repeat  bg-cover bg-center  h-screen  "  >
+            <div className='border flex flex-col gap-4 h-full justify-center  !p-8 bg-[#00000067]'>
+              <h1 className='md:text-5xl text-3xl  font-[Orbitron] text-white font-bold'>Maximize Your Revenue</h1>
+              <p className='text-white md:text-2xl text-lg font-semibold'>Keep your truck loaded and profitable</p>
+              <button className="bg-[var(--primary-color)] text-white px-8 !py-2 skew-x-[20deg] md:w-[250px] w-[180px]">
+                <span className="block skew-x-[-20deg] font-bold">
+                  <a href="">WHY CHOOSE US</a>
+                </span>
+              </button>
+
+            </div>
+         
+
+
+          </CarouselItem>
+
       
       
             
