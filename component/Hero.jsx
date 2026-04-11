cf8e6f87768840a7348570284866453f6bac1567
Fassam Khan <muhammadfassam465@gmail.com>
2026-04-11 14:33:24 +0500

    changes in hero section

--- component/Hero.jsx
+++ component/Hero.jsx
@@ -20,8 +20,8 @@ const Hero = () => {
          
           
           <CarouselItem className="pl-4  w-full bg-[url('/hero1.jpeg')] p-3 bg-no-repeat bg-cover bg-center   h-[400px]  "  >
-            <div className='border flex flex-col gap-4 h-full justify-center items-start   bg-[#00000067] md:!pl-16 items-center'>
-              <h1 className='md:text-5xl text-[22px]  font-[Orbitron] text-white font-bold'>YOUR ROUTE TO SUCCESS</h1>
+            <div className='border flex flex-col gap-4 h-full justify-center md:items-start   bg-[#00000067] md:!pl-16 items-center '>
+              <h1 className='md:text-5xl text-[20px]  font-[Orbitron] text-white font-bold'>YOUR ROUTE TO SUCCESS</h1>
               <p className='text-white md:text-2xl text-lg font-semibold'>Premium Truck Dispatch Services</p>
               <button className="bg-[var(--primary-color)] text-white px-8 !py-2 skew-x-[20deg] md:w-[250px] w-[180px]">
                 <span className="block skew-x-[-20deg] font-bold">
@@ -34,14 +34,13 @@ const Hero = () => {
 
 
           </CarouselItem>
-           
-          <CarouselItem className="w-full bg-[url('/hero2.jpeg')] p-3 bg-no-repeat  bg-cover bg-center   h-[400px]  "  >
-            <div className='border flex flex-col gap-4 h-full justify-center items-start   bg-[#00000067] md:!pl-16 items-center'>
-              <h1 className='md:text-5xl text-[22px]  font-[Orbitron] text-white font-bold'>Find High-Paying Loads 24/7</h1>
-              <p className='text-white md:text-2xl text-lg font-semibold'>Expert dispatchers working arround the clock</p>
+          <CarouselItem className="pl-4  w-full bg-[url('/hero2.jpeg')] p-3 bg-no-repeat bg-cover bg-center   h-[400px]  "  >
+            <div className='border flex flex-col gap-4 h-full justify-center md:items-start   bg-[#00000067] md:!pl-16 items-center '>
+              <h1 className='md:text-5xl text-[20px]  font-[Orbitron] text-white font-bold'>YOUR ROUTE TO SUCCESS</h1>
+              <p className='text-white md:text-2xl text-lg font-semibold'>Premium Truck Dispatch Services</p>
               <button className="bg-[var(--primary-color)] text-white px-8 !py-2 skew-x-[20deg] md:w-[250px] w-[180px]">
                 <span className="block skew-x-[-20deg] font-bold">
-                  <a href="">VIEW SERVICES</a>
+                  <a href="">GET STARTED TODAY</a>
                 </span>
               </button>
 
@@ -50,14 +49,13 @@ const Hero = () => {
 
 
           </CarouselItem>
-
-          <CarouselItem className="w-full bg-[url('/hero3.jpeg')] p-3 bg-no-repeat  bg-cover bg-center   h-[400px] "  >
-            <div className='border flex flex-col gap-4 h-full justify-center items-start   bg-[#00000067] md:!pl-16 items-center'>
-              <h1 className='md:text-5xl text-[22px]  font-[Orbitron] text-white font-bold'>Maximize Your Revenue</h1>
-              <p className='text-white md:text-2xl text-lg font-semibold'>Keep your truck loaded and profitable</p>
+          <CarouselItem className="pl-4  w-full bg-[url('/hero3.jpeg')] p-3 bg-no-repeat bg-cover bg-center   h-[400px]  "  >
+            <div className='border flex flex-col gap-4 h-full justify-center md:items-start   bg-[#00000067] md:!pl-16 items-center '>
+              <h1 className='md:text-5xl text-[20px]  font-[Orbitron] text-white font-bold'>YOUR ROUTE TO SUCCESS</h1>
+              <p className='text-white md:text-2xl text-lg font-semibold'>Premium Truck Dispatch Services</p>
               <button className="bg-[var(--primary-color)] text-white px-8 !py-2 skew-x-[20deg] md:w-[250px] w-[180px]">
                 <span className="block skew-x-[-20deg] font-bold">
-                  <a href="">WHY CHOOSE US</a>
+                  <a href="">GET STARTED TODAY</a>
                 </span>
               </button>
 
@@ -66,7 +64,7 @@ const Hero = () => {
 
 
           </CarouselItem>
-
+        
       
       
             
