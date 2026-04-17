0e7262b3876a35b251ee2f9ed646c0d5e259be4c
Fassam Khan <muhammadfassam465@gmail.com>
2026-04-17 12:29:51 +0500

    Wquipment added successfully

--- component/Equipment.jsx
+++ component/Equipment.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const Equipment = () => {
   return (
-    <div>
+    <div className='!py-4'>
         <div className="wrapper">
               {/* Heading  */}
               <div className='text-center' id='faq'>
@@ -19,10 +19,41 @@ const Equipment = () => {
                 </div>
 
                 {/* Card Container  */}
-                <div>
+                <div className='grid md:grid-cols-5 grid-cols-2 gap-4 !mt-4'>
                     {/* Card  */}
-                    <div className='w-[150px] h-[100px]'> hi
-                        <Image src={'/truck1.png'} alt='DryVan' width={150} height={100}/>
+                    <div className='md:w-[250px] rounded-xl  border border-gray-300 hover:border-[var(--primary-color)] transition-all hover:scale-105 focus-within:scale-105 flex flex-col gap-4 text-center !p-4'> 
+                        <Image src={'/truck1.png'} alt='DryVan' width={250} height={100}/>
+                        <p className='text-lg font-bold font-[Orbitron]'>Dry Van</p>
+
+                    </div>
+                        {/* Card  */}
+                        <div className='md:w-[250px] rounded-xl  border border-gray-300 hover:border-[var(--primary-color)] transition-all hover:scale-105 focus-within:scale-105 flex flex-col gap-4 text-center !p-4'> 
+                        <Image src={'/truck2.jfif'} alt='DryVan' width={250} height={100}/>
+                        <p className='text-lg font-bold font-[Orbitron]'>Reefer</p>
+
+                    </div>
+                        {/* Card  */}
+                        <div className='md:w-[250px] rounded-xl  border border-gray-300 hover:border-[var(--primary-color)] transition-all hover:scale-105 focus-within:scale-105 flex flex-col gap-4 text-center !p-4'> 
+                        <Image src={'/truck3.jfif'} alt='DryVan' width={250} height={100}/>
+                        <p className='text-lg font-bold font-[Orbitron]'>Flatbed</p>
+
+                    </div>
+                        {/* Card  */}
+                        <div className='md:w-[250px] rounded-xl  border border-gray-300 hover:border-[var(--primary-color)] transition-all hover:scale-105 focus-within:scale-105 flex flex-col gap-4 text-center !p-4'> 
+                        <Image src={'/truck4.png'} alt='DryVan' width={250} height={100}/>
+                        <p className='text-lg font-bold font-[Orbitron]'>Step Deck</p>
+
+                    </div>
+                        {/* Card  */}
+                        <div className='md:w-[250px] rounded-xl  border border-gray-300 hover:border-[var(--primary-color)] transition-all hover:scale-105 focus-within:scale-105 flex flex-col gap-4 text-center !p-4'> 
+                        <Image src={'/truck5.jfif'} alt='DryVan' width={250} height={100}/>
+                        <p className='text-lg font-bold font-[Orbitron]'>Power Only</p>
+
+                    </div>
+                        {/* Card  */}
+                        <div className='md:w-[250px] rounded-xl  border border-gray-300 hover:border-[var(--primary-color)] transition-all hover:scale-105 focus-within:scale-105 flex flex-col gap-4 text-center !p-4'> 
+                        <Image src={'/truck6.jfif'} alt='DryVan' width={250} height={100}/>
+                        <p className='text-lg font-bold font-[Orbitron]'>Hotshot</p>
 
                     </div>
                 </div>
