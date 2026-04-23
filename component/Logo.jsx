56c9b8cb64607917ffd88968013e6641c9bdb7e4
Fassam Khan <muhammadfassam465@gmail.com>
2026-04-23 21:01:08 +0500

    header updated

--- component/Logo.jsx
+++ component/Logo.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Logo = () => {
   return (
+    <Link href={'/'} >
     <div className='  relative h-[45px]  font-[Orbitron] '>
         <p className='text-white font-bold text-2xl '>FIRST BRIDGE</p>
         <span className='absolute bottom-[-4px] text-[var(--primary-color)] text-[12px] tracking-[1.5px] font-semibold '>DISPATCH SERVICES</span>
       
     </div>
+    </Link>
   )
 }
 
