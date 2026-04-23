437088b89d581a4203daad3a41ffbb230a2cd7df
Fassam Khan <muhammadfassam465@gmail.com>
2026-04-23 20:53:16 +0500

    changing in card

--- app/freetools/[slug]/page.jsx
+++ app/freetools/[slug]/page.jsx
@@ -4,6 +4,7 @@ import Header from '@/component/Header'
 import { tools } from '@/lib/tools'
 import { notFound } from 'next/navigation'
 import { useParams } from 'next/navigation'
+import Link from 'next/link'
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -20,6 +21,9 @@ const Page = () => {
   console.log(tool)
 
   const ToolComponent = tool.component
+
+  const related = tools.filter(tool => tool.slug !== tool)
+  .slice(0, 4);
 
 
 
@@ -59,6 +63,27 @@ const Page = () => {
         {<ToolComponent />}
 
         </div>
+        {/* Related tools  */}
+        <div>
+
+        <div className='border-b border-gray-300  md:!mt-20'>
+            <h4 className='md:text-2xl  font-bold '>Related Tools</h4>
+        </div>
+        {/* tools  */}
+        <div className='grid md:grid-cols-4 grid-cols-2 !mt-6'>
+          {related.map((e,index)=>{
+            return  <Link href={`/freetools/${e.slug}` } key={index}>
+            <div className='flex flex-col md:w-[300px] rounded shadow-xl !p-4 bg-white gap-4 border-t-2 border-[var(--primary-color)]'>
+            <p className='text-2xl'>{e.icon}</p>
+            <h4 className='text-[#004e89] font-[Orbitron] font-bold'>{e.name}</h4>
+            <p>{e.description} </p>
+            <button className='cursor-pointer bg-[var(--primary-color)] font-bold  text-white w-full !py-2'>USE CALCULATOR</button>
+          </div>
+        </Link>
+          })}
+        </div>
+        </div>
+
 
       </div>
       <Footer />
