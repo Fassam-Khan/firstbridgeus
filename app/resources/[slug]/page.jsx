79266f4e50a11d1e61150fada5386bd4a13810b6
Fassam Khan <muhammadfassam465@gmail.com>
2026-04-30 21:36:54 +0500

    blog added successfully

--- app/resources/[slug]/page.jsx
+++ app/resources/[slug]/page.jsx
@@ -1,14 +1,52 @@
-import React from 'react'
+"use client"
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'next/navigation'
+import Header from '@/component/Header'
+import Footer from '@/component/Footer'
+import Link from 'next/link'
 
-const page = ({params}) => {
+const page = () => {
+  const parmas = useParams()
 
-  const {slug}= params
+  const [blog, setdata] = useState({})
+
+  const getBlog = async () => {
+    try {
+      const res = await fetch("/api/blog")
+      const data = await res.json()
+      const singleBlog = data.find(t => t.slug === parmas.slug)
+      console.log(singleBlog)
+      await setdata(singleBlog)
+
+
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  useEffect(() => {
+    getBlog()
+  }, [])
 
 
   return (
     <div>
-      <h4>Comming Soon .....</h4>
+      <Header />
+      <main className='bg-[#f7f9fb]'>
+        <div className="wrapper md:w-[800px] w-full !m-auto">
+          <Link href={'/'} className='text-[var(--primary-color)] font-semibold'>Back to blog</Link>
+
       
+        <div>
+          <div
+            dangerouslySetInnerHTML={{ __html: blog.content }}
+          />
+        </div>
+        </div>
+      </main>
+      <Footer />
+
     </div>
   )
 }
