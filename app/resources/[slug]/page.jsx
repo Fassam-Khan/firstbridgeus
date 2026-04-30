b9110d247440118ca4c56f14403132694c0402bb
Fassam Khan <muhammadfassam465@gmail.com>
2026-04-30 22:07:02 +0500

    blog completed

--- app/resources/[slug]/page.jsx
+++ app/resources/[slug]/page.jsx
@@ -4,19 +4,22 @@ import { useParams } from 'next/navigation'
 import Header from '@/component/Header'
 import Footer from '@/component/Footer'
 import Link from 'next/link'
-
+import { ArrowLeft } from 'lucide-react'
 const page = () => {
   const parmas = useParams()
 
   const [blog, setdata] = useState({})
+  const [Loading, setLoading] = useState(true)
 
   const getBlog = async () => {
     try {
+
       const res = await fetch("/api/blog")
       const data = await res.json()
       const singleBlog = data.find(t => t.slug === parmas.slug)
       console.log(singleBlog)
       await setdata(singleBlog)
+      setLoading(false)
 
 
     } catch (error) {
@@ -33,16 +36,25 @@ const page = () => {
   return (
     <div>
       <Header />
-      <main className='bg-[#f7f9fb]'>
-        <div className="wrapper md:w-[800px] w-full !m-auto">
-          <Link href={'/'} className='text-[var(--primary-color)] font-semibold'>Back to blog</Link>
 
-      
-        <div>
-          <div
-            dangerouslySetInnerHTML={{ __html: blog.content }}
-          />
-        </div>
+      <main className='bg-[#f7f9fb]'>
+        {Loading && (
+          <p>Loading...</p>
+        )}
+        <div className="wrapper md:w-[800px] w-full !m-auto">
+          <Link href={'/'} className='text-[var(--primary-color)] font-semibold flex gap-2'>
+            <ArrowLeft className="w-5 h-5" />
+            Back to blog</Link>
+
+          <div className='!mt-6'>
+            <h2 className='font-[Orbitron] md:text-4xl text-lg font-bold'>{blog.title}</h2>
+          </div>
+
+          <div className='!mt-6'>
+            <div
+              dangerouslySetInnerHTML={{ __html: blog.content }}
+            />
+          </div>
         </div>
       </main>
       <Footer />
