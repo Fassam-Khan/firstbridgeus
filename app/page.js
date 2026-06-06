2c63b069586f2fd2f68361d10acd5ac085fbd2aa
Fassam Khan <muhammadfassam465@gmail.com>
2026-06-06 22:08:21 +0500

    Final Change

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ import Weekly from "@/component/Weekly";
 import Equipment from "@/component/Equipment";
 import Testimonial from "@/component/Testimonial";
 import Contact from "@/component/Contact";
-import Chatbot from "@/component/Chatbot";
+
 
 export default function Home() {
   return (
@@ -28,7 +28,7 @@ export default function Home() {
    <Whysus/>
    <Faq/>
    <Contact/>
-   <Chatbot/>
+  
   
    <Footer/>
   
